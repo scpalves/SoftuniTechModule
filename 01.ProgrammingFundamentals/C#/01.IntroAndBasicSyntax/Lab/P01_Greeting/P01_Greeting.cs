7826d9f980733593d8e0dd941e201797e6ae21f6
RadostinV-Stoychev <RadostinV.Stoychev@gmail.com>
2017-09-22 23:35:35 +0300

    test

---
+++ 01.ProgrammingFundamentals/C#/01.IntroAndBasicSyntax/Lab/P01_Greeting/P01_Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P01_Greeting
+{
+    class P01_Greeting
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
