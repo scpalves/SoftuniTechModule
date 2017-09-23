7499f56faed51547870d9fcca3191d86b333a711
RadostinV-Stoychev <radostinv.stoychev@gmail.com>
2017-09-23 06:16:56 +0300

    Uploading Exercises C#

---
+++ 01.ProgrammingFundamentals/C#/01.IntroAndBasicSyntax/Exercises/P01_DebitCardNumber/P01_DebitCardNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_DebitCardNumber
+{
+    using System;
+    class P01_DebitCardNumber
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a:D4} {b:D4} {c:D4} {d:D4}");
+        }
+    }
+}
