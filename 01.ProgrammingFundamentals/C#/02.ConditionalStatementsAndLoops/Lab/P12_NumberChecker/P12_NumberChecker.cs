bf6023aecaf7e82df88ba6353bf1e767bcd1403a
RadostinV-Stoychev <radostinv.stoychev@gmail.com>
2017-09-25 07:24:44 +0300

    ConditionalStatementsLoops Lab upload

---
+++ 01.ProgrammingFundamentals/C#/02.ConditionalStatementsAndLoops/Lab/P12_NumberChecker/P12_NumberChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P12_NumberChecker
+{
+    class P12_NumberChecker
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine("It is a number.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
